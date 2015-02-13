b1aaa1a20cebcd936eb902a4f2be24ce6bb16e89
crow <utmcrow@gmail.com>
2015-02-13 20:41:32 +0200

    basic parsing

--- parser.py
+++ parser.py
@@ -3,14 +3,21 @@ __author__ = 'crow'
 from os import listdir
 from os.path import isfile, join
 import os.path, time
+from datetime import datetime ,timedelta
 
 def main():
 
-    mypath = '/Users/utmcrow/Dropbox/test/'
+    past = int(time.time())
+
+    mypath = '/Users/utmcrow/Dropbox/images/'
     onlyfiles = [ f for f in listdir(mypath) if isfile(join(mypath,f)) ]
     for file in onlyfiles:
         print "last modified: %s" % time.ctime(os.path.getmtime(mypath+file))
         print "created: %s" % time.ctime(os.path.getctime(mypath+file))
+        print "diff: %s" % timedelta(seconds=past-os.path.getmtime(mypath+file))
+
+        (mode, ino, dev, nlink, uid, gid, size, atime, mtime, ctime) = os.stat(mypath+file)
+        print "last modified: %s" % time.ctime(mtime)
     pass
 
 if __name__ == "__main__":
